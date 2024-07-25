380c16e2de23a830cd49a41aa03a954e3703ee80
josue rodrigues da silva <josuerodrigues42@outlook.com>
2024-07-24 22:23:22 -0300

    projeto de venda

---
+++ script.js
@@ -0,0 +1,8 @@
+function add () {
+    let inputProduto = document.getElementById("inputProduto").value;
+    let inputMarca = document.getElementById("inputMarca").value;
+    let inputEstoque = document.getElementById("inputEstoque").value;
+    let inputCusto = document.getElementById("inputCusto").value;
+    let inputVenda = document.getElementById("inputVenda").value;
+    console.log(inputProduto, inputMarca, Estoque, Custo, Venda);
+}
